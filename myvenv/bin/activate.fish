94e17f6ded17b03fd87631c305e6f88dbaec11e6
smh3223 <alsk1976@kookmin.ac.kr>
2019-12-18 20:47:56 +0900

    Export EXE

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelavent variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/user/djangogirls/myvenv"
+set -gx VIRTUAL_ENV "/home/user/SWP_AD_Project/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
